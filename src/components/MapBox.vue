0d3b9a2a7602d1aebd7c513239d6bfec3b264b1d
Mc-Waves <malvinajongbah@gmail.com>
2020-08-20 09:27:38 +0000

    first commit

---
+++ src/components/MapBox.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="maps">
+    <div class="mapbox" id="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+export default {
+  name: "MapBox",
+  data() {
+    return {};
+  },
+  methods: {
+    renderMap() {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiYWpvbmdiYWgiLCJhIjoiY2tiYTNoNjhkMGZtbDJ6cXFoYmZxMHFreCJ9.uzXroTEEy2RjJH7rKEjQhw";
+      const map = new mapboxgl.Map({
+        container: "mapContainer",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-1.5716114, 6.674503499999999],
+        zoom: 10,
+      });
+
+      const marker = new mapboxgl.Marker().setLngLat([-1.5716114, 6.674503499999999]).addTo(map);
+    },
+  },
+  mounted() {
+    this.renderMap();
+  },
+};
+</script>
+
+<style>
+.mapbox {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import firestore from 'firebase/store'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzTa-5jhncAiydCpGzzhfUVW2kq1GEJac",
+  authDomain: "geo-app-85e94.firebaseapp.com",
+  databaseURL: "https://geo-app-85e94.firebaseio.com",
+  projectId: "geo-app-85e94",
+  storageBucket: "geo-app-85e94.appspot.com",
+  messagingSenderId: "211218373576",
+  appId: "1:211218373576:web:74135b18b2f99664bb3efd",
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore()
